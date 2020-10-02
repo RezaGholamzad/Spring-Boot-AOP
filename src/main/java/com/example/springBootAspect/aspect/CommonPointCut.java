07fd6681b0816b549f6b632109ad26d5f749644d
reza <gholamzadreza@gmail.com>
2020-10-02 19:47:29 +0330

    update spirng AOP

--- src/main/java/com/example/springBootAspect/aspect/CommonPointCut.java
+++ src/main/java/com/example/springBootAspect/aspect/CommonPointCut.java
@@ -1,12 +1,7 @@
 package com.example.springBootAspect.aspect;
 
-import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 
-@Aspect
-@Component
 public class CommonPointCut {
 
     @Pointcut("execution(* com.example.springBootAspect.controller.EmployeeController.*(..)))")
